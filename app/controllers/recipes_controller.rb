1d01de1a72709ff9d0ad5d4322472888165df09c
helgakern <helgakern@gmail.com>
2020-04-30 20:33:21 -0700

    inserted root route

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,7 @@
 class RecipesController < ApplicationController
+
     def index
+        @recipes = [ 'Strogonofe', 'Filé de frango a milanesa', 'Salmão Assado', 'Bacalhau as Natas']
     end
+
 end
