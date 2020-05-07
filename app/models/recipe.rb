415425595ae34345fa8c3c6f7c1a482d9b0181e4
helgakern <helgakern@gmail.com>
2020-05-06 19:00:27 -0700

    created custom filter

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,10 @@ class Recipe < ApplicationRecord
     def light?
         calories < 100
     end
+
+    def self.created_at
+        where('created_at <= ?', Time.now).order('created_at DESC')
+    end
+
+
 end
